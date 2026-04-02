ba8c70e31c51e9bdf16729c3991d8eafe0ac7887
Pratik Jha <pratik.jma@gmail.com>
2026-04-03 00:09:32 +0530

    Added headless mode for CI

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -4,17 +4,21 @@ import io.cucumber.java.Before;
 import io.cucumber.java.After;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
 
     public static WebDriver driver;
-
     @Before
     public void setup() {
         System.out.println("Launching browser...");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
